9fba55f5fdca284f0023526e75db0bb43df16208
John Mansell <jmansell90@gmail.com>
2018-12-28 22:59:18 +0000

    Init

---
+++ flirt/cute.js
@@ -0,0 +1,7 @@
+alert("You're really cute, did you know that? ;)")
+var response = confirm ("Don't worry, I'm not going to hack your shit.  Let's see if you're as adventurous as I am.  Care to chat in person?")
+if (response){
+	alert("I'll be right there :D")
+}else{
+	alert("Ok, ok, I'll leave you alone now (If this pops again, sorry, glitches happen!)")
+}
